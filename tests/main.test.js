8828705e0523672ea1ada6a6e39ea0ca7c642c25
William Killerud <william.killerud@schibsted.com>
2024-07-29 12:19:03 +0200

    feat: sync with built-in sink from eik/core

--- tests/main.test.js
+++ tests/main.test.js
@@ -25,7 +25,7 @@ tap.test('Sink() - .read() - File exists', async (t) => {
     const type = 'text/plain';
     const writable = await sink.write(path, type);
     const readable = Readable.from(['Hello, World!']);
-    t.resolves(pipeline(readable, writable));
+    await t.resolves(pipeline(readable, writable));
 
     const file = await sink.read(path);
     t.equal(file.mimeType, type);
@@ -40,32 +40,21 @@ tap.test('Sink() - .read() - File exists', async (t) => {
     t.end();
 });
 
-tap.test('Sink() - .read() - File does not exist', (t) => {
+tap.test('Sink() - .read() - File does not exist', async (t) => {
     const sink = new Sink();
-    t.rejects(sink.read('/does/not/exist.txt'));
+    await t.rejects(sink.read('/does/not/exist.txt'));
     t.end();
 });
 
-tap.test('Sink() - .delete() - File exists', async (t) => {
+tap.test('Sink() - .delete()', async (t) => {
     const sink = new Sink();
     const path = '/mem/foo/bar.txt';
     const writable = await sink.write(path, 'text/plain');
     const readable = Readable.from(['Hello, World!']);
 
-    t.resolves(pipeline(readable, writable));
-    t.resolves(sink.delete(path));
+    await t.resolves(pipeline(readable, writable));
+    await t.resolves(sink.delete(path));
 
-    t.rejects(
-        sink.delete(path),
-        'Second call to delete should reject if file is successfully deleted',
-    );
-
-    t.end();
-});
-
-tap.test('Sink() - .delete() - File does not exist', (t) => {
-    const sink = new Sink();
-    t.rejects(sink.delete('/does/not/exist.txt'));
     t.end();
 });
 
@@ -75,13 +64,13 @@ tap.test('Sink() - .exist() - File exists', async (t) => {
     const writable = await sink.write(path, 'text/plain');
     const readable = Readable.from(['Hello, World!']);
 
-    t.resolves(pipeline(readable, writable));
-    t.resolves(sink.exist(path));
+    await t.resolves(pipeline(readable, writable));
+    await t.resolves(sink.exist(path));
     t.end();
 });
 
-tap.test('Sink() - .exist() - File does not exist', (t) => {
+tap.test('Sink() - .exist() - File does not exist', async (t) => {
     const sink = new Sink();
-    t.rejects(sink.exist('/does/not/exist.txt'));
+    await t.rejects(sink.exist('/does/not/exist.txt'));
     t.end();
 });
